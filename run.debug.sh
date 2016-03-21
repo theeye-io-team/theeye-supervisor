ee763355c6cc75490bbffa88dabbcea21142484e
Facundo Gonzalez <facundo.siquot@gmail.com>
2016-03-21 17:54:15 -0300

    remove no_monitoring

--- run.debug.sh
+++ run.debug.sh
@@ -6,6 +6,7 @@ fi
 
 export DEBUG
 
-NO_MONITORING='true' NODE_ENV='localdev' ./node_modules/.bin/nodemon --harmony_proxies --ignore 'uploads/*' $PWD/core/main.js
+#NO_MONITORING='true' NODE_ENV='localdev' ./node_modules/.bin/nodemon --harmony_proxies --ignore 'uploads/*' $PWD/core/main.js
+NODE_ENV='localdev' ./node_modules/.bin/nodemon --harmony_proxies --ignore 'uploads/*' $PWD/core/main.js
 
 exit 0;
