9c9d4f96b22b3586e4560f029b5b22ecbdf69977
Facugon <facugon@interactar.com>
2017-07-14 16:54:51 -0300

    just lint code

--- core/controllers/dstat.js
+++ core/controllers/dstat.js
@@ -1,26 +1,25 @@
 'use strict';
 
-var json = require('../lib/jsonresponse');
-var HostStats = require('../entity/host/stats').Entity;
-var NotificationService = require('../service/notification');
-var logger = require('../lib/logger')('controller:dstat');
-var router = require('../router');
-var config = require('config');
+const config = require('config');
+const logger = require('../lib/logger')('controller:dstat');
+const router = require('../router');
+const json = require('../lib/jsonresponse');
+const HostStats = require('../entity/host/stats').Entity;
+const NotificationService = require('../service/notification');
+const ResourceManager = require('../service/resource');
+const elastic = require('../lib/elastic');
 
-var ResourceManager = require('../service/resource');
-var elastic = require('../lib/elastic');
-
-module.exports = function(server, passport){
+module.exports = function (server, passport) {
   server.post('/:customer/dstat/:hostname',[
     passport.authenticate('bearer',{session:false}),
     router.requireCredential('agent'),
     router.resolve.customerNameToEntity({required:true}),
     router.ensureCustomer,
     router.resolve.hostnameToHost({})
-  ],controller.create);
+  ], controller.create)
 }
 
-function parseStats (stats) {
+const parseStats = (stats) => {
   if (!isNaN(parseInt(stats.cpu_idle))) {
     if (!stats.cpu_used) {
       stats.cpu_used = 100 - stats.cpu_idle;
@@ -29,23 +28,47 @@ function parseStats (stats) {
   return stats;
 }
 
-var controller = {
+const elasticSubmit = (data) => {
+  logger.data('dstat %j', data);
+  const key = config.elasticsearch.keys.host.stats;
+  elastic.submit(data.customer_name,key,data);
+  NotificationService.sendSNSNotification(data,{
+    topic: 'host-stats',
+    subject: 'dstat_update'
+  });
+}
+
+/**
+ *
+ * @summary store agent dstat collected data
+ * @param {Host} req.host
+ * @param {Customer} req.customer
+ * @param {Object} req.body.dstat
+ *
+ * the monitoring information is being display via web interface
+ * and stored in elasticsearch if available
+ *
+ */
+const controller = {
   create (req, res, next) {
-    logger.log('Handling host dstat data');
+    logger.log('Handling host dstat data')
 
-    var host = req.host,
-      customer = req.customer,
-      stats = parseStats(req.body.dstat);
+    const host = req.host
+    const customer = req.customer
+    const stats = parseStats(req.body.dstat)
 
-    if (!host) return res.send(404,'host not found');
-    if (!stats) return res.send(400,'no stats supplied');
+    if (!host) return res.send(404,'invalid host. not found')
+    if (!stats) return res.send(400,'stats are required')
 
     HostStats.findOne({
       host_id: host._id,
       type:'dstat'
-    },function(error,dstat){
-      if(error) return logger.error(error);
-      if(dstat == null) {
+    },(error,dstat) => {
+      if (error) {
+        return logger.error(error)
+      }
+
+      if (dstat == null) {
         logger.log('creating host dstat');
         HostStats.create(host,'dstat',stats);
       } else {
@@ -59,38 +82,17 @@ var controller = {
       }
     });
 
-    /**
-    ResourceManager.findHostResources(host,options,(err,resource)=>{
-      if(err||!resource)return;
-      var handler = new ResourceManager(resource);
-      handler.handleState({
-        last_update: new Date(),
-        state: 'normal'
-      });
-    });
-    */
-
     logger.log('resending dstat data');
 
-    var data = {
-      'timestamp': (new Date()).getTime(),
-      'date': (new Date()).toISOString(),
-      'customer_name': customer.name,
-      'hostname': host.hostname,
-      'stats': req.body.dstat,
-      'type': 'host-stats'
-    };
-
-    logger.data('dstat %j', data);
-
-    var key = config.elasticsearch.keys.host.stats;
-    elastic.submit(customer.name,key,data);
-
-    NotificationService.sendSNSNotification(data,{
-      topic: 'host-stats',
-      subject: 'dstat_update'
-    });
+    elasticSubmit({
+      timestamp: (new Date()).getTime(),
+      date: (new Date()).toISOString(),
+      customer_name: customer.name,
+      hostname: host.hostname,
+      stats: req.body.dstat,
+      type: 'host-stats'
+    })
 
     return res.send(200);
   }
-};
+}
