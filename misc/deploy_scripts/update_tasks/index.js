17f0567954806946ae58939a2e304b77fef21b28
Facugon <facugon@interactar.com>
2017-07-25 00:20:35 -0300

    update deploy script task . add customer to task schema

--- misc/deploy_scripts/update_tasks/index.js
+++ misc/deploy_scripts/update_tasks/index.js
@@ -1,26 +1,30 @@
 "use strict"
 
-const ObjectId = require('mongoose').Schema.Types.ObjectId
+const ObjectId = require('mongoose').Types.ObjectId
 const AppRoot = __dirname + '/../../../core'
 const lodash = require('lodash')
 //const debug = require('debug')('eye:deploy:cleanup_events')
 const Workspace = require(AppRoot + '/workspace')
 
 Workspace((err,App) => {
-  App.Task.find({}, (err,tasks) => {
+  App.Task.find({
+    //_id: ObjectId('5814bf1ce490ae1200a24674') 
+  }, (err,tasks) => {
     var done = lodash.after(tasks.length, () => {
       console.log('done');
       process.exit()
     })
     tasks.forEach(task => {
-      task.customer = ObjectId(task.customer_id)
-      task.save(() => {
+      let id = ObjectId(task.customer_id)
+      task.customer = id
+      task.save((err, result) => {
         if (err) {
           console.error('ERROR')
           console.error(task)
           console.error(err)
         } else {
-          console.log('task updated');
+          //console.log(result.customer)
+          console.log(`task ${task._id} updated`)
         }
         done()
       })
