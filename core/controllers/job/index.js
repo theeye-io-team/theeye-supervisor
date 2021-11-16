87d4de708fc53d85f53a30a6ed830ae9e2b8b5d6
facugon <facugon@theeye.io>
2021-11-16 15:35:39 -0300

    tasks queue controller moved to task router

--- core/controllers/job/index.js
+++ core/controllers/job/index.js
@@ -2,11 +2,9 @@ const crud = require('./crud')
 const create = require('./create')
 const acl = require('./acl')
 const lifecycle = require('./lifecycle')
-const queue = require('./queue')
 
 module.exports = (server) => {
   create(server)
-  queue(server)
   crud(server)
   acl(server)
   lifecycle(server)
