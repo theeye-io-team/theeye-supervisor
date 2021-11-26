002320e6bb1cda885d3a8d0c5a7adb9cb726d0cd
Facundo <facugon@theeye.io>
2021-11-25 23:43:19 -0300

    feat: Workflow copy, export and import (#8)
    * workflow recipe. app core improves
    
    * standard
    
    * add db validation errors handler
    
    * workflow unscheduler (on remove workflow)
    
    * task destroy method.
    
    remove everything linked to a task.
    
    * stop throwing errors when events are not found
    
    * workflow CRUD methods updated
    
    * crud controllers backward compatible
    
    * added api version control using headers
    
    * ErrorHandler.ClientError dependency added

--- core/controllers/workflow/index.js
+++ core/controllers/workflow/index.js
@@ -5,9 +5,11 @@ const acl = require('./acl')
 const triggers = require('./triggers')
 const integrations = require('./integrations')
 const scheduler = require('./scheduler')
+const recipe = require('./recipe')
 
 module.exports = (server) => {
   job(server)
+  recipe(server)
   acl(server)
   crud(server)
   graph(server)
