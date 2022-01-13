f62760736ae2d4cc2ef4a1f87535ba00b4d1a81b
Facundo <facugon@theeye.io>
2022-01-13 08:06:44 -0300

    File indicator (#12)
    Co-authored-by: Ezequiel Korenblum <ezequiel@losko.com.ar>

--- core/constants/indicator.js
+++ core/constants/indicator.js
@@ -1,10 +1,12 @@
 
-const SHORT_TYPE_CHART = 'chart'
-const SHORT_TYPE_TEXT = 'text'
+const SHORT_TYPE_CHART    = 'chart'
+const SHORT_TYPE_TEXT     = 'text'
 const SHORT_TYPE_PROGRESS = 'progress'
-const SHORT_TYPE_COUNTER = 'counter'
+const SHORT_TYPE_COUNTER  = 'counter'
+const SHORT_TYPE_FILE     = 'file'
 
-exports.SHORT_TYPE_CHART     = SHORT_TYPE_CHART
+exports.SHORT_TYPE_CHART    = SHORT_TYPE_CHART
 exports.SHORT_TYPE_TEXT     = SHORT_TYPE_TEXT
 exports.SHORT_TYPE_PROGRESS = SHORT_TYPE_PROGRESS
 exports.SHORT_TYPE_COUNTER  = SHORT_TYPE_COUNTER 
+exports.SHORT_TYPE_FILE     = SHORT_TYPE_FILE
