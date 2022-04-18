0b4a47ab1d1a107d4fc0dcdfa8abf574562277f9
Facundo <facugon@theeye.io>
2022-04-18 16:11:49 -0300

    feat: fetch user data for every workflow job (#43)

--- core/service/events/triggered-by-job-settings.js
+++ core/service/events/triggered-by-job-settings.js
@@ -1,7 +1,7 @@
 const App = require('../../app')
 const ObjectId = require('mongoose').Types.ObjectId
 
-module.exports = (job) => {
+module.exports = async (job) => {
   let user // the owner of the execution
   let dynamic_settings = null // if the event was emitted by a job
 
@@ -11,13 +11,20 @@ module.exports = (job) => {
     }
 
     if (job.user_id) {
+      const gwusers = await App.gateway.user.fetch(
+        [ job.user_id ],
+        { customer_id: job.customer_id }
+      )
+
       user = {
         _id: new ObjectId(job.user_id),
-        id: job.user_id.toString()
+        id: job.user_id.toString(),
+        username: gwusers[0].username,
+        email: gwusers[0].email
       }
     }
   } else {
-    user = App.user
+    user = {}
   }
 
   return { user, dynamic_settings }
