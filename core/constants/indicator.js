134a9d3c7399c7dd09ec6bc063109cd411889f5a
Ezequiel Korenblum <ezequiel@losko.com.ar>
2022-07-04 15:45:39 -0300

    html indicator (#64)

--- core/constants/indicator.js
+++ core/constants/indicator.js
@@ -1,12 +1,14 @@
 
 const SHORT_TYPE_CHART    = 'chart'
 const SHORT_TYPE_TEXT     = 'text'
+const SHORT_TYPE_HTML     = 'html'
 const SHORT_TYPE_PROGRESS = 'progress'
 const SHORT_TYPE_COUNTER  = 'counter'
 const SHORT_TYPE_FILE     = 'file'
 
 exports.SHORT_TYPE_CHART    = SHORT_TYPE_CHART
 exports.SHORT_TYPE_TEXT     = SHORT_TYPE_TEXT
+exports.SHORT_TYPE_HTML     = SHORT_TYPE_HTML
 exports.SHORT_TYPE_PROGRESS = SHORT_TYPE_PROGRESS
 exports.SHORT_TYPE_COUNTER  = SHORT_TYPE_COUNTER 
 exports.SHORT_TYPE_FILE     = SHORT_TYPE_FILE
