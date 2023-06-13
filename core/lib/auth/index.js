139f2aef0e4de6c4110e8f8a04633c20a68b681f
facugon <facugon@theeye.io>
2023-06-13 17:05:39 -0300

    feat: better jwt tokens encryption (#88)
    * use jwt pub/priv keys
    
    * fix: capture error job result not found
    
    * always verify jwt token
    
    * validate parameters
    
    * remove unsed token verification logic
    
    Token must be verified against the Gateway MS.
    
    * feat: timestampt added to log stream
    
    * add commet

--- core/lib/auth/index.js
+++ core/lib/auth/index.js
@@ -1,26 +1,26 @@
 const passport = require('passport')
 const BearerStrategy = require('passport-http-bearer').Strategy
 const BasicStrategy = require('passport-http').BasicStrategy
-const jwt = require('jsonwebtoken')
 
 const logger = require('../logger')(':auth')
 const http = require('http')
 const https = require('https')
 const config = require('config').authentication
 
-const request = config.protocol === 'https' ? https.request : http.request
-
-const requestOptions = {
-  host: config.api.host,
-  port: config.api.port
-}
-
 module.exports = {
   initialize () {
-    const basicStrategy = new BasicStrategy(async (client_id, client_secret, done) => {
+
+    const request = config.protocol === 'https' ? https.request : http.request
+
+    const requestOptions = {
+      host: config.api.host,
+      port: config.api.port
+    }
+
+    const basicStrategyMiddleware = new BasicStrategy(async (client_id, client_secret, done) => {
       logger.log('new connection [basic]')
       try {
-        let response = await createSession(client_id, client_secret)
+        let response = await createGatewaySession(client_id, client_secret)
         if (response.statusCode === 200) {
           let json = JSON.parse(response.rawBody)
           return done(null, json.access_token)
@@ -38,16 +38,16 @@ module.exports = {
       }
     })
 
-    const bearerStrategy = new BearerStrategy(async (token, done) => {
+    const bearerStrategyMiddleware = new BearerStrategy(async (token, done) => {
       try {
+        //
+        // WARNING!! Integration Tokens cannot be verified using JWT.verify
+        //
+        // 2023/09/06. Most of the Integration tokens were issued with the wrong expiration date and are all expired. Tokens can be verified against living session that exists in db and cache. This is being checked using the Gateway API.
+        //
         logger.log('new connection [bearer]')
-        //const decoded = verifyToken(token)
-        //if (decoded) {
-        //  console.log(decoded)
-        //}
-
-        //sessionVerify(token).then(profile => {})
-        const response = await fetchProfile(token)
+        // @TODO use cache
+        const response = await fetchGatewayProfile(token)
         if (response.statusCode === 200) {
           let profile = JSON.parse(response.rawBody)
           return done(null, profile)
@@ -64,8 +64,8 @@ module.exports = {
       }
     })
 
-    passport.use(basicStrategy)
-    passport.use(bearerStrategy)
+    passport.use(basicStrategyMiddleware)
+    passport.use(bearerStrategyMiddleware)
 
     const bearerMiddleware = (req, res, next) => {
       passport.authenticate('bearer', (err, profile) => {
@@ -86,97 +86,58 @@ module.exports = {
       }, { session: false })(req, res, next)
     }
 
+    const fetchGatewayProfile = (token) => {
+      return new Promise((resolve, reject) => {
+        let reqOpts = Object.assign({
+          path: `${config.api.path.profile}?access_token=${token}`,
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          }
+        }, requestOptions)
+
+        const req = request(reqOpts, res => {
+          let str = ''
+          res.on('data', d => { if (d) { str += d } })
+          res.on('end', () =>  {
+            res.rawBody = str
+            resolve(res)
+          })
+        })
+        req.on('error', error => reject(error))
+        //req.write(JSON.stringify(payload))
+        req.end()
+      })
+    }
+
+    const createGatewaySession = (user, pass) => {
+      return new Promise((resolve, reject) => {
+        const creds = Buffer.from(`${user}:${pass}`).toString('base64')
+        let reqOpts = Object.assign({
+          path: config.api.path.login,
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Basic ${creds}`
+          }
+        }, requestOptions)
+
+        const req = request(reqOpts, function (res) {
+          let str = ''
+          res.on('data', d => { if (d) { str += d } })
+          res.on('end', function () {
+            res.rawBody = str
+            resolve(res)
+          })
+        })
+        req.on('error', error => reject(error))
+        req.end()
+      })
+    }
+
     return { bearerMiddleware }
   }
 }
 
-const verifyToken = (token) => {
-  let decoded
-  try {
-    decoded = jwt.verify(token, config.secret, {})
-  } catch (jwtErr) {
-    logger.log(jwtErr)
-  }
-  logger.log(decoded)
-  return decoded
-}
-
-const fetchProfile = (token) => {
-  return new Promise((resolve, reject) => {
-    let reqOpts = Object.assign({
-      path: `${config.api.path.profile}?access_token=${token}`,
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
-    }, requestOptions)
-
-    const req = request(reqOpts, res => {
-      let str = ''
-      res.on('data', d => { if (d) { str += d } })
-      res.on('end', () =>  {
-        res.rawBody = str
-        resolve(res)
-      })
-    })
-    req.on('error', error => reject(error))
-    //req.write(JSON.stringify(payload))
-    req.end()
-  })
-}
-
-const createSession = (user, pass) => {
-  return new Promise((resolve, reject) => {
-    const creds = Buffer.from(`${user}:${pass}`).toString('base64')
-    let reqOpts = Object.assign({
-      path: config.api.path.login,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': `Basic ${creds}`
-      }
-    }, requestOptions)
-
-    const req = request(reqOpts, function (res) {
-      let str = ''
-      res.on('data', d => { if (d) { str += d } })
-      res.on('end', function () {
-        res.rawBody = str
-        resolve(res)
-      })
-    })
-    req.on('error', error => reject(error))
-    req.end()
-  })
-}
-
-const sessionVerify = (token) => {
-  return new Promise((resolve, reject) => {
-    let reqOpts = Object.assign({
-      path: `${config.api.path.verify}?access_token=${token}`,
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
-    }, requestOptions)
-
-    const req = request(reqOpts, res => {
-      let str = ''
-      res.on('data', d => { if (d) { str += d } })
-      res.on('end', () =>  {
-        try {
-          resolve(JSON.parse(str))
-        } catch (error) {
-          reject(error)
-        }
-      })
-    })
-
-    req.on('error', error => reject(error))
-    //req.write(JSON.stringify(payload))
-    req.end()
-  })
-}
--- core/service/gateway/token.js
+++ core/service/gateway/token.js
@@ -3,11 +3,13 @@ const jwt = require('jsonwebtoken')
 
 module.exports = {
   create (context) {
+    const key = config.authentication.rs256.priv
     return jwt.sign(
       { context },
-      config.gateway.secret, // our Private Key
+      key, // our Private Key
       {
-        expiresIn: 60 // seconds
+        expiresIn: 60, // seconds
+        algorithm: "RS256"
       }
     )
   }
