add70d7fe9709705e00dbf077d041216b26f6ab0
facugon <facugon@theeye.io>
2023-08-15 14:32:08 -0300

    use permission to filter db

--- core/router/db-filter.js
+++ core/router/db-filter.js
@@ -1,5 +1,5 @@
 const dbFilter = require('../lib/db-filter')
-const { ForbiddenError } = require('../lib/error-handler')
+const { ForbiddenError, ServerError } = require('../lib/error-handler')
 
 module.exports = (options) => {
   return (req, res, next) => {
@@ -7,14 +7,23 @@ module.exports = (options) => {
       const customer = req.customer
       const query = req.query
       const filter = dbFilter(query)
-      filter.where.customer_id = customer.id
+      filter.where.$or = [
+        { customer_id: customer.id },
+        { customer: customer.id },
+      ]
 
       if (!req.permissions) {
         throw new ForbiddenError()
       }
 
-      const acl = req.permissions.map(p => p.value)
-      filter.where.acl = { $in: acl }
+      if (req.permissions !== true) {
+        if (!Array.isArray(req.permissions)) {
+          throw new ServerError('Bad permissions definition')
+        }
+        const acl = req.permissions.map(p => p.value)
+        filter.where.acl = { $in: acl }
+      }
+
       req.dbQuery = filter
       return next()
     } catch (err) {
